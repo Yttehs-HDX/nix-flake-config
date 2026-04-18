685d951ab11b566b742ccd9d9a65844a0020b47d
Shetty Yttehs <shetty.yttehs@outlook.com>
2026-04-18 15:31:23 +0800

    chore(obsidian): switch to the nixpkgs-unstable version of Obsidian

--- modules/packages/obsidian/home.nix
+++ modules/packages/obsidian/home.nix
@@ -1,1 +1,9 @@
-{ ... }: { ... }: { programs.obsidian.enable = true; }
+{ ... }:
+{ pkgs, inputs, ... }:
+let
+  system = pkgs.stdenv.hostPlatform.system;
+  unstablePkgs = import inputs.nixpkgs-unstable {
+    inherit system;
+    config.allowUnfree = true;
+  };
+in { home.packages = [ unstablePkgs.obsidian ]; }
